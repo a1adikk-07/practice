5baf1b3108abe58f0dfc9947b5d1bce0658db88d
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-11-11 13:56:30 +0000

    Deploying to gh-pages from @ a1adikk-07/practice@bf833a794a5730c6e87b823d3bbf3c38c89d109b 🚀

---
+++ index.2c17a36b.js
@@ -0,0 +1,2 @@
+const e={};document.querySelector(".login-form").addEventListener("input",(function(n){const{name:o,value:t}=n.target;e[o]=t,console.log(e)}));
+//# sourceMappingURL=index.2c17a36b.js.map
---
+++ index.9771b289.js
@@ -0,0 +1,2 @@
+!function(){var e={};document.querySelector(".login-form").addEventListener("input",(function(n){var o=n.target,t=o.name,r=o.value;e[t]=r,console.log(e)}))}();
+//# sourceMappingURL=index.9771b289.js.map
