6a0d5d330abde47a42d6a5731a732adf6bbe172a
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2023-11-11 15:52:38 +0200

    task done

---
+++ src/js/login.js
@@ -0,0 +1,28 @@
+// Якщо імейл і пароль користувача збігаються, зберігай дані з форми при сабміті
+// у локальне сховище і змінюй кнопку login на logout і роби поля введення
+// недоступними для змін.
+
+// При перезавантаженні сторінки, якщо користувач залогінений, ми маємо бачити logout-кнопку
+// та недоступні для зміни поля з даними користувача.
+// Клік по кнопці logout повертає все до початкового вигляду і видаляє дані користувача
+// З локального сховища.
+
+// Якщо введені дані не збігаються з потрібними даними, викликати аlert і
+// повідомляти про помилку.
+
+const USER_DATA = {
+  email: "user@mail.com",
+  password: "secret",
+};
+
+const data = {};
+
+const userForm = document.querySelector(".login-form");
+
+userForm.addEventListener("input", onSaveData)
+
+function onSaveData(event) {
+    const { name, value } = event.target;
+    data[name] = value;
+    console.log(data);
+}
