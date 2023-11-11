8a272cd8232a30a87d6b85ad4d9ab7579d81b000
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-11-11 09:27:33 -0500

    task2 done

--- src/js/login.js
+++ src/js/login.js
@@ -11,18 +11,37 @@
 // повідомляти про помилку.
 
 const USER_DATA = {
-  email: "user@mail.com",
-  password: "secret",
+  email: 'user@mail.com',
+  password: 'secret',
 };
 
 const data = {};
 
-const userForm = document.querySelector(".login-form");
+const userForm = document.querySelector('.login-form');
+const userBtn = document.querySelector('.login-btn');
+const inputLogin = document.querySelector("input[name='email']");
+const inputPassword = document.querySelector("input[name='password']");
 
-userForm.addEventListener("input", onSaveData)
+userForm.addEventListener('input', onSaveData);
+userForm.addEventListener('submit', onLogin);
+if (localStorage.getItem('form-data')) checkLog();
 
 function onSaveData(event) {
-    const { name, value } = event.target;
-    data[name] = value;
-    console.log(data);
+  const { name, value } = event.target;
+  data[name] = value;
+  console.log(data);
+}
+function onLogin(event) {
+  event.preventDefault();
+  if (!data.email || !data.password) return alert('Fill all fields');
+  if (data.email !== USER_DATA.email || data.password !== USER_DATA.password)
+    return alert('Incorrect data');
+  checkLog();
+  localStorage.setItem('form-data', JSON.stringify(data));
+  userForm.reset();
+}
+function checkLog() {
+  userBtn.textContent = 'Logout';
+  inputLogin.setAttribute('readonly', true);
+  inputPassword.setAttribute('readonly', true);
 }
