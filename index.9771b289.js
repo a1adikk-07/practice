6aaf6a77ac2b2144a7fd8623d7126f87531098b9
Alexander221991 <138162632+Alexander221991@users.noreply.github.com>
2023-11-11 14:30:21 +0000

    Deploying to gh-pages from @ a1adikk-07/practice@a18a6cfe857ca7cc572846715387425db1e91aa7 🚀

--- index.9771b289.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={};document.querySelector(".login-form").addEventListener("input",(function(n){var o=n.target,t=o.name,r=o.value;e[t]=r,console.log(e)}))}();
-//# sourceMappingURL=index.9771b289.js.map
