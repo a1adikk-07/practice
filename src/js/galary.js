5e11828e6575297936c484b4aa10904eb56850f6
sivlvy <vladislavsidenko@ukr.net>
2023-11-25 15:28:19 +0200

    task-6-2

--- src/js/galary.js
+++ src/js/galary.js
@@ -1,5 +1,36 @@
 import { UnsplashAPI } from './UnsplashAPI';
+import { createGalleryCard } from './createMarkup';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+
+const refs = {
+	gallery: document.querySelector('.js-gallery'),
+	container: document.getElementById('tui-pagination-container'),
+};
+
+const options = {
+	totalItems: 0,
+	itemsPerPage: 12,
+	visiblePages: 5,
+	page: 1,
+};
+
+const pagination = new Pagination(refs.container, options);
+
+const page = pagination.getCurrentPage();
+
 const apiService = new UnsplashAPI();
-apiService
-  .getImagePopular(1)
-  .then(({ results, total }) => console.log(results, total));
+apiService.getImagePopular(page).then(({ results, total }) => {
+	const markup = createGalleryCard(results);
+	refs.gallery.innerHTML = markup;
+	pagination.reset(total);
+});
+
+pagination.on('afterMove', event => {
+	const currentPage = event.page;
+	console.log(currentPage);
+	apiService.getImagePopular(currentPage).then(({ results, total }) => {
+		const markup = createGalleryCard(results);
+		refs.gallery.innerHTML = markup;
+	});
+});
